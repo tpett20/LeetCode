62c683fe862eeb9d9d041b2abd8cd3cf813d4bb0
tpett20 <thomaspettersen20@gmail.com>
2023-06-01 16:10:30 -0400

    add Problem 9 solution

---
+++ 9.js
@@ -0,0 +1,21 @@
+/* 
+9. Palindrome Number
+Given an integer x, return true if x is a palindrome, and false otherwise. 
+*/
+
+/**
+ * @param {number} x
+ * @return {boolean}
+ */
+var isPalindrome = function(x) {
+    x = x.toString()
+    let y = ''
+    for (let i = x.length - 1; i >= 0; i--) {
+        y += x[i]
+    }
+    return x === y ? true : false
+};
+
+// TEST CASE
+console.log(isPalindrome(121))
+console.log(isPalindrome(10))
