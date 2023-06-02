608b660abd33b24f01541dbfce60ec0658aba2f8
tpett20 <thomaspettersen20@gmail.com>
2023-06-02 16:43:15 -0400

    solve Problem 412 in Python

---
+++ 412.py
@@ -0,0 +1,23 @@
+# 412. FizzBuzz
+
+# Given an integer n, return a string array answer (1-indexed) where:
+
+# answer[i] == "FizzBuzz" if i is divisible by 3 and 5.
+# answer[i] == "Fizz" if i is divisible by 3.
+# answer[i] == "Buzz" if i is divisible by 5.
+# answer[i] == i (as a string) if none of the above conditions are true.
+
+def fizzBuzz(n):
+    list = []
+    for i in range(1, n+1):
+        if i % 3 == 0 and i % 5 == 0:
+            list.append('FizzBuzz')
+        elif i % 3 == 0:
+            list.append('Fizz')
+        elif i % 5 == 0:
+            list.append('Buzz')
+        else: 
+            list.append(f"{i}")
+    return list
+
+print(fizzBuzz(15))
