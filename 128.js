244df35ca0c7bc367c7d681168b2abaaf379b02a
tpett20 <thomaspettersen20@gmail.com>
2023-07-28 10:54:41 -0400

    clean up code for Problem 128 algo

--- 128.js
+++ 128.js
@@ -4,53 +4,42 @@
 
 var longestConsecutive = function (nums) {
     if (!nums.length) return 0
-    const map1 = {}
-    const map2 = {}
+    const posMap = {}
+    const negMap = {}
     for (let num of nums) {
         if (num >= 0) {
-            map1[num] = true
+            posMap[num] = true
         } else {
-            map2[Math.abs(num)] = true
+            negMap[Math.abs(num)] = true
         }
     }
-    let count = 1
-    let highCount = 0
-    let borderCount = 0
-    let border = false
-    for (let key in map2) {
-        if (key === '1') {
-            border = true
-            borderCount = 1
+    function getCounts(map, borderNum) {
+        let count = 1
+        let highCount = 0
+        let borderCount = 0
+        let border = false
+        for (let key in map) {
+            if (key === borderNum.toString()) {
+                border = true
+                borderCount = 1
+            }
+            if (map[key] && map[parseInt(key) + 1]) {
+                count++
+                if (border) borderCount++
+            } else {
+                count = 1
+                border = false
+            }
+            if (count > highCount) {
+                highCount = count
+            }
         }
-        if (map2[key] && map2[parseInt(key) + 1]) {
-            count++
-            if (border) borderCount++
-        } else {
-            count = 1
-            border = false
-        }
-        if (count > highCount) {
-            highCount = count
-        }
+        return [highCount, borderCount]
     }
-    count = 1
-    border = false
-    for (let key in map1) {
-        if (key === '0') {
-            border = true
-            borderCount++
-        }
-        if (map1[key] && map1[parseInt(key) + 1]) {
-            count++
-            if (border) borderCount++
-        } else {
-            count = 1
-            border = false
-        }
-        if (count > highCount) {
-            highCount = count
-        }
-    }
+    let posCounts = getCounts(posMap, 0)
+    let negCounts = getCounts(negMap, 1)
+    let highCount = posCounts[0] > negCounts[0] ? posCounts[0] : negCounts[0]
+    let borderCount = posCounts[1] + negCounts[1]
     return highCount > borderCount ? highCount : borderCount
 };
 
