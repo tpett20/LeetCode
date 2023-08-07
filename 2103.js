81eb52ee365a03539df02a395de4bf160e907258
tpett20 <thomaspettersen20@gmail.com>
2023-08-07 10:40:42 -0400

    solve Problem 2103

---
+++ 2103.js
@@ -0,0 +1,34 @@
+// 2103. Rings and Rods
+/*
+There are n rings and each ring is either red, green, or blue. The rings are distributed across ten rods labeled from 0 to 9.
+You are given a string rings of length 2n that describes the n rings that are placed onto the rods. Every two characters in rings forms a color-position pair that is used to describe each ring where:
+- The first character of the ith pair denotes the ith ring's color ('R', 'G', 'B').
+- The second character of the ith pair denotes the rod that the ith ring is placed on ('0' to '9').
+For example, "R3G2B1" describes n == 3 rings: a red ring placed onto the rod labeled 3, a green ring placed onto the rod labeled 2, and a blue ring placed onto the rod labeled 1.
+Return the number of rods that have all three colors of rings on them.
+*/
+
+var countPoints = function(rings) {
+    const map = {}
+    let count = 0
+    for (let i = 0; i < rings.length - 1; i += 2) {
+        let color = rings[i]
+        let rod = rings[i + 1]
+        if (!map[rod]) {
+            map[rod] = {}
+        }
+        map[rod][color] = true
+    }
+    for (let rod in map) {
+        let colorCount = 0
+        for (let color in map[rod]) {
+            colorCount++
+        }
+        if (colorCount === 3) {
+            count++
+        }
+    }
+    return count
+};
+
+console.log(countPoints("B0B6G0R6R0R6G9"))
