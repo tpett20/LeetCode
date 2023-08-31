3d19bedd5124d1d20290edbc8648342f1cb4a7b9
tpett20 <thomaspettersen20@gmail.com>
2023-08-31 10:02:23 -0400

    add preplanned pseudocode to the Problem 228 algo

--- 228.js
+++ 228.js
@@ -25,3 +25,30 @@ var summaryRanges = function(nums) {
 };
 
 console.log(summaryRanges([0,2,3,4,6,8,9]))
+
+
+// With Preplanned Pseudocode
+var pseudocode = function(nums) {
+    if (nums[0] === undefined) return nums
+    // create an output array to store all the ranges
+    const ranges = []
+    // create a sub array to hold each range that will fill the output array
+    // initialize it with the first value of nums
+    let range = [nums[0]]
+    // iterate over each number in nums, starting with the second
+    for (let i = 1; i < nums.length; i++) {
+        // if the current number is equal to the previous number + 1, set the second value of the sub array (range[1]) equal to the current number, making it the closing paramater of the range
+        if (nums[i] === nums[i - 1] + 1) {
+            range[1] = nums[i] 
+        // else (if the current number is not equal to previous + 1), push the values of the current sub array to the output array as a string in the specified format 
+        } else {
+            ranges.push(range[1] === undefined ? `${range[0]}` : `${range[0]}->${range[1]}`)
+            // reset the sub array to hold the current number 
+            range = [nums[i]]
+        }
+    }
+    // push the last sub array to the output array
+    ranges.push(range[1] === undefined ? `${range[0]}` : `${range[0]}->${range[1]}`)
+    // return the output array
+    return ranges
+};
