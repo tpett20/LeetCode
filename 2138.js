d4e21083024301d67a06fcfc6ce0e59d37087eb8
tpett20 <thomaspettersen20@gmail.com>
2023-09-23 09:09:24 -0400

    solve Problem 2138.js

---
+++ 2138.js
@@ -0,0 +1,28 @@
+// 2138. Divide a String Into Groups of Size k
+/*
+A string s can be partitioned into groups of size k using the following procedure:
+- The first group consists of the first k characters of the string, the second group consists of the next k characters of the string, and so on. Each character can be a part of exactly one group.
+- For the last group, if the string does not have k characters remaining, a character fill is used to complete the group.
+Note that the partition is done so that after removing the fill character from the last group (if it exists) and concatenating all the groups in order, the resultant string should be s.
+Given the string s, the size of each group k and the character fill, return a string array denoting the composition of every group s has been divided into, using the above procedure.
+*/
+
+var divideString = function(s, k, fill) {
+    const arr = []
+    let str = ''
+    for (let char of s) {
+        str += char
+        if (str.length === k) {
+            arr.push(str)
+            str = ''
+        }
+    }
+    if (str) {
+        str += fill.repeat(k - str.length)
+        arr.push(str)
+    }
+    return arr
+};
+
+console.log(divideString('abcdefghi', 3, 'x'))
+console.log(divideString('abcdefghij', 3, 'x'))
