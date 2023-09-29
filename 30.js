638bac6942135d35528ccc044000a7c3bb5bf154
tpett20 <thomaspettersen20@gmail.com>
2023-09-29 10:56:19 -0400

    solve Problem 30

--- 30.js
+++ 30.js
@@ -1,5 +1,4 @@
 // 30. Substring with Concatenation of All Words
-// 🚫 INCOMPLETE - Passes 160 / 179 Testcases
 /*
 You are given a string s and an array of strings words. All the strings of words are of the same length.
 A concatenated substring in s is a substring that contains all the strings of any permutation of words concatenated.
@@ -10,37 +9,28 @@ Return the starting indices of all the concatenated substrings in s. You can ret
 var findSubstring = function(s, words) {
     const output = []
     let wordLen = words[0].length
-    if (wordLen * words.length > s.length) return output
-    const map = {}
-    let checker = { i: 0, len: words.length }
+    let totalWordsLen = wordLen * words.length
+    if (totalWordsLen > s.length) return output
+    const wordsMap = { len: words.length }
+    let checker = { len: words.length }
     for (let word of words) {
-        map[word] = map[word] ? map[word] + 1 : 1
-        checker[word] = map[word]
+        wordsMap[word] = wordsMap[word] ? wordsMap[word] + 1 : 1
+        checker[word] = wordsMap[word]
     }
-    let resetChecker = true
-    for (let i = 0; i <= s.length - wordLen; i++) {
+    for (let i = 0; i <= s.length - totalWordsLen; i++) {
         let str = s.slice(i, i + wordLen)
-        if (map[str]) {
-            while (!checker[str]) {
-                let oldStr = s.slice(checker.i, checker.i + wordLen)
-                checker[oldStr]++
-                checker.len++
-                checker.i += wordLen
-            }
+        let j = i
+        while (checker[str]) {
             checker[str]--
             checker.len--
-            i += (wordLen - 1)
-            resetChecker = false
-        } else {
-            if (!resetChecker) {
-                checker = { ...map, i: i + 1, len: words.length }
-                resetChecker = true
-            } else checker.i = i + 1
+            j += wordLen
+            str = s.slice(j, j + wordLen)
+            if (checker.len === 0) output.push(i)
         }
-        if (checker.len === 0) output.push(checker.i)
+        checker = { ...wordsMap }
     }
     return output
 };
 
-console.log(findSubstring('barfoothefoobarman', ['foo', 'bar']))
-console.log('INCORRECT', findSubstring('aaaaaaaaaaaaaa', ['aa', 'aa']))
+console.log(findSubstring('barfoofoobarthefoobarman', ['foo', 'bar', 'the']))
+console.log(findSubstring('aaaaaaaaaaaaaa', ['aa', 'aa']))
