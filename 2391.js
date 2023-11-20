14043be7a7dd441874e3f641b41e5fb1f766fb26
tpett20 <thomaspettersen20@gmail.com>
2023-11-20 10:13:31 -0500

    solve Problem 2391

---
+++ 2391.js
@@ -0,0 +1,31 @@
+// 2391. Minimum Amount of Time to Collect Garbage
+/*
+You are given a 0-indexed array of strings garbage where garbage[i] represents the assortment of garbage at the ith house. garbage[i] consists only of the characters 'M', 'P' and 'G' representing one unit of metal, paper and glass garbage respectively. Picking up one unit of any type of garbage takes 1 minute.
+You are also given a 0-indexed integer array travel where travel[i] is the number of minutes needed to go from house i to house i + 1.
+There are three garbage trucks in the city, each responsible for picking up one type of garbage. Each garbage truck starts at house 0 and must visit each house in order; however, they do not need to visit every house.
+Only one garbage truck may be used at any given moment. While one truck is driving or picking up garbage, the other two trucks cannot do anything.
+Return the minimum number of minutes needed to pick up all the garbage.
+*/
+
+var garbageCollection = function(garbage, travel) {
+    let gTime = 0
+    let pTime = 0
+    let mTime = 0
+    for (let i = garbage.length - 1; i >= 0; i--) {
+        for (let type of garbage[i]) {
+            if (type === 'G') gTime++
+            else if (type === 'P') pTime++
+            else if (type === 'M') mTime++
+        }
+        let travelTime = travel[i - 1]
+        if (travelTime) {
+            if (gTime) gTime += travelTime
+            if (pTime) pTime += travelTime
+            if (mTime) mTime += travelTime
+        }
+    }
+    return gTime + pTime + mTime
+};
+
+console.log(garbageCollection(["G","P","GP","GG"], [2,4,3]))
+console.log(garbageCollection(["MMM","PGM","GP"], [3,10]))
