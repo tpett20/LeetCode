739810130756360a75bc69a014794c9fdfd543f9
tpett20 <thomaspettersen20@gmail.com>
2024-02-02 14:47:50 -0500

    add Problem 1291 description

--- 1291.py
+++ 1291.py
@@ -1,3 +1,7 @@
+# 1291. Sequential Digits
+# An integer has sequential digits if and only if each digit in the number is one more than the previous digit.
+# Return a sorted list of all the integers in the range [low, high] inclusive that have sequential digits.
+
 class Solution:
     def sequentialDigits(self, low: int, high: int) -> list[int]:
         def makeSeqNum(start, length):
