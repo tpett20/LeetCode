dd126dca204447c6e7140c864ef0f27e7acaf1b6
tpett20 <thomaspettersen20@gmail.com>
2024-02-26 10:46:34 -0500

    solve Problem 2651

---
+++ 2651.js
@@ -0,0 +1,11 @@
+// 2651. Calculate Delayed Arrival Time
+// You are given a positive integer arrivalTime denoting the arrival time of a train in hours, and another positive integer delayedTime denoting the amount of delay in hours.
+// Return the time when the train will arrive at the station.
+// Note that the time in this problem is in 24-hours format.
+
+var findDelayedArrivalTime = function(arrivalTime, delayedTime) {
+    return (arrivalTime + delayedTime) % 24
+};
+
+console.log(findDelayedArrivalTime(15, 5))
+console.log(findDelayedArrivalTime(13, 11))
