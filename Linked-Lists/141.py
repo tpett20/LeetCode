6b7f365b79ccc24f23d93c76a600bd811eec3636
tpett20 <thomaspettersen20@gmail.com>
2024-03-06 09:59:45 -0500

    solve Problem 141 with O(1) memory

--- Linked-Lists/141.py
+++ Linked-Lists/141.py
@@ -3,14 +3,30 @@
 # There is a cycle in a linked list if there is some node in the list that can be reached again by continuously following the next pointer. Internally, pos is used to denote the index of the node that tail's next pointer is connected to. Note that pos is not passed as a parameter.
 # Return true if there is a cycle in the linked list. Otherwise, return false.
 
+class ListNode:
+    def __init__(self, x):
+        self.val = x
+        self.next = None
+
 class Solution:
-    def hasCycle(head):
-        walker = head
-        while (walker):
-            check = hasattr(walker, 'checked')
-            if check == False: 
-                walker.checked = True
-            else: 
+    def hasCycle(self, head: [ListNode]) -> bool:
+        if not head:
+            return False
+        slow = head
+        fast = head.next
+        while fast and fast.next:
+            if slow == fast:
                 return True
-            walker = walker.next
+            slow = slow.next
+            fast = fast.next.next
         return False
+
+test_case = ListNode(3)
+pos_node = ListNode(2)
+test_case.next = pos_node
+pos_node.next = ListNode(0)
+pos_node.next.next = ListNode(-4)
+pos_node.next.next.next = pos_node
+
+s = Solution()
+print(s.hasCycle(test_case))
