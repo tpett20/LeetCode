98f7ef47cd90c5f848f52efc7d255d8832e7c10d
tpett20 <thomaspettersen20@gmail.com>
2024-03-07 09:54:39 -0500

    solve Problem 876 in Python

---
+++ Linked-Lists/876.py
@@ -0,0 +1,40 @@
+# 876. Middle of the Linked List
+# Given the head of a singly linked list, return the middle node of the linked list.
+# If there are two middle nodes, return the second middle node.
+
+class ListNode:
+    def __init__(self, val = 0, next = None):
+        self.val = val
+        self.next = next
+
+class Solution:
+    def middleNode(self, head: [ListNode]) -> [ListNode]:
+        length = 0
+        walker = head
+        while walker:
+            length += 1
+            walker = walker.next
+        mid = length // 2 + 1
+        idx = 1
+        walker = head
+        while idx != mid:
+            walker = walker.next
+            idx += 1
+        return walker
+
+node5 = ListNode(5)
+node4 = ListNode(4, node5)
+node3 = ListNode(3, node4)
+node2 = ListNode(2, node3)
+test_case = ListNode(1, node2)
+
+s = Solution()
+midNode = s.middleNode(test_case)
+
+# Print midNode as a string
+display = ""
+walker = midNode
+while walker:
+    display += str(walker.val) + " -> "
+    walker = walker.next
+print(display + "None")
