cdef28769749e771293863cfe2b14b6a3080b01e
tpett20 <thomaspettersen20@gmail.com>
2024-06-29 17:14:52 -0400

    solve Problem 1038, solution from Jun 25, 2024

---
+++ Trees/1038.js
@@ -0,0 +1,25 @@
+// 1038. Binary Search Tree to Greater Sum Tree
+/*
+Given the root of a Binary Search Tree (BST), convert it to a Greater Tree such that every key of the original BST is changed to the original key plus the sum of all keys greater than the original key in BST.
+As a reminder, a binary search tree is a tree that satisfies these constraints:
+- The left subtree of a node contains only nodes with keys less than the node's key.
+- The right subtree of a node contains only nodes with keys greater than the node's key.
+- Both the left and right subtrees must also be binary search trees.
+*/
+
+var bstToGst = function(root) {
+    let total = 0
+    function traverse(node) {
+        if (node.right) {
+            traverse(node.right)
+        }
+        node.val += total
+        total = node.val
+        if (node.left) {
+            traverse(node.left)
+        }
+    }
+    
+    traverse(root)
+    return root
+};
