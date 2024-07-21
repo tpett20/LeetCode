a652addd870a41381912564906b2d901598df6cd
tpett20 <thomaspettersen20@gmail.com>
2024-07-21 13:53:49 -0400

    solve Problem 1837

---
+++ 1837.js
@@ -0,0 +1,15 @@
+// 1837. Sum of Digits in Base K
+// Given an integer n (in base 10) and a base k, return the sum of the digits of n after converting n from base 10 to base k.
+// After converting, each digit should be interpreted as a base 10 number, and the sum should be returned in base 10.
+
+var sumBase = function(n, k) {
+    const convNum = n.toString(k)
+    console.log(convNum)
+    let sum = 0
+    for (const digit of convNum) {
+        sum += parseInt(digit)
+    }
+    return sum
+};
+
+console.log(sumBase(34, 6))
