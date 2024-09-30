e806d483482c017a86e267df3f632f0d870d691d
tpett20 <thomaspettersen20@gmail.com>
2024-09-29 20:49:03 -0400

    solve Problem 1876

---
+++ 1876.js
@@ -0,0 +1,24 @@
+// 1876. Substrings of Size Three with Distinct Characters
+/*
+A string is good if there are no repeated characters.
+Given a string s​​​​​, return the number of good substrings of length three in s​​​​​​.
+Note that if there are multiple occurrences of the same substring, every occurrence should be counted.
+A substring is a contiguous sequence of characters in a string.
+*/
+
+var countGoodSubstrings = function(s) {
+    let count = 0
+    for (let i = 0; i < s.length - 2; i++) {
+        if (
+            s[i] !== s[i + 1] && 
+            s[i] !== s[i + 2] && 
+            s[i + 1] !== s[i + 2]
+        ) {
+            count++
+        }
+    }
+    return count
+};
+
+console.log(countGoodSubstrings("aababcabc"))
+console.log(countGoodSubstrings("letsgomets"))
